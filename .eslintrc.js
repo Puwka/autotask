373cabd2aa592254e91c6f25754d6e725fc355ca
Puwka <gorokhov.inc@gmail.com>
2019-05-04 12:32:17 +0300

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   root: true,
   env: {
@@ -10,6 +11,12 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'arrow-parens': 'off',
+    'no-param-reassign': 'off',
+    'comma-dangle': 'off',
+    indent: ['error', 4],
+    'consistent-return': 'off',
+    'no-underscore-dangle': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint',
